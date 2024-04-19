5f8099eebf9f2696d73bf15beb9dc784976d11cd
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-04-19 01:50:00 -0300

    Ajustes

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -29,41 +29,38 @@ while True:
             saldo += valor
             print(f"Valor depositado: R$ {valor:.2f}")
             extrato += f"Depósito de R$ {valor:.2f}\n"
-            # print(extrato)
-            # print(f"Saldo: R$ {saldo:.2f}")
 
     elif opcao == 2:
         print(f"Opção escolhida: {opcao} - Saque.")
-        # desenvolver lógica de saque
 
         if numero_saques >= 3:
-            print("Não foi possível realizar o saque. Você excedeu o limite diário de 3 saques.")
+            print("Não é  possível realizar o saque. Você excedeu o limite diário de 3 saques.")
         else:
-            valor = float(input("Qual o valor do saque? "))
+            print(f"Saldo: R$ {saldo:.2f}")
+            valor_saque = float(input("Qual o valor do saque? "))
 
-            if valor <= 500 and saldo >= valor:
-                print(f"Saque efetuado no valor de R$ {valor:.2f}")
-                extrato += f"Saque de R$ {valor:.2f}\n"
-                saldo -= valor
-                numero_saques += 1
-                # print(extrato)
-                # print(f"Saldo: R$ {saldo:.2f}")
-            elif valor > 500:
-                print("Operação falhou. O valor solicitado excede o limite.")
-            elif valor > saldo:
+            if valor_saque > saldo:
                 print("Operação falhou. Você não tem saldo suficiente.")
+            
+            elif valor_saque > 500:
+                print("Operação falhou. O valor solicitado excede o limite.")
 
+            elif valor_saque <= 500 and saldo >= valor_saque:
+                print(f"Saque efetuado no valor de R$ {valor_saque:.2f}")
+                extrato += f"Saque de R$ {valor_saque:.2f}\n"
+                saldo = saldo - valor_saque
+                numero_saques += 1
 
     elif opcao == 3:
         print(f"Opção escolhida: {opcao} - Extrato.")
-        # desenvolver lógica de extrato
         print("Extrato".center(31, "="))
         print(extrato if extrato else "Não foram realizadas movimentações.")
+        print(f"Saldo atual: R$ {valor:.2f}")
+        print("Fim do Extrato".center(31, "="))
 
     elif opcao == 0:
         print(f"Opção escolhida: {opcao} - Sair.")
         break
-        # sair
 
     else:
         print("Opção inválida. Selecione novamente a opção desejada.")
