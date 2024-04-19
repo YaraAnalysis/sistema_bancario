c2a96f63bd16c9d90be12ed503d256ef42e6cea7
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-04-19 00:21:03 -0300

    Lógica do depósito

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,4 +1,5 @@
 menu = '''
+===============================
 Bem vindo à sua conta bancária.
 Ecolha uma opção:
 
@@ -9,23 +10,41 @@ Ecolha uma opção:
 Escolha a sua opção: 
 '''
 
+saldo = 0
+extrato = ""
+valor_maximo_saque = 500
+limite_saques = 3
+numero_saques = 0
+
 while True:
 
     opcao = int(input(menu))
 
     if opcao == 1:
-        print(f"Opção escolhida: {opcao}.")
-        # desenvolver lógica de depósito
+        print(f"Opção escolhida: {opcao} - Depósito.")
+
+        valor = float(input("Qual o valor do depósito? "))
+
+        if valor > 0:
+            saldo += valor
+            print(f"Valor depositado: R$ {valor:.2f}")
+            extrato += f"Depósito de R$ {valor:.2f}"
+            # print(extrato)
+            # print(f"Saldo: R$ {saldo:.2f}")
+
     elif opcao == 2:
-        print(f"Opção escolhida: {opcao}.")
+        print(f"Opção escolhida: {opcao} - Saque.")
         # desenvolver lógica de saque
+
     elif opcao == 3:
-        print(f"Opção escolhida: {opcao}.")
+        print(f"Opção escolhida: {opcao} - Extrato.")
         # desenvolver lógica de extrato
+
     elif opcao == 0:
-        print(f"Opção escolhida: {opcao}.")
+        print(f"Opção escolhida: {opcao} - Sair.")
         break
         # sair
+
     else:
         print("Opção inválida. Selecione novamente a opção desejada.")
 
