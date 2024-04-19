716eb67e1d10232c9b6af457a1c17fbc58eb819c
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-04-18 23:32:02 -0300

    estrutura do if principal

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -2,9 +2,30 @@ menu = '''
 Bem vindo à sua conta bancária.
 Ecolha uma opção:
 
-Escolha a sua opção
 [1] Depositar
 [2] Sacar
 [3] Extrato
 [0] Sair
+Escolha a sua opção: 
 '''
+
+while True:
+
+    opcao = int(input(menu))
+
+    if opcao == 1:
+        print(f"Opção escolhida: {opcao}.")
+        # desenvolver lógica de depósito
+    elif opcao == 2:
+        print(f"Opção escolhida: {opcao}.")
+        # desenvolver lógica de saque
+    elif opcao == 3:
+        print(f"Opção escolhida: {opcao}.")
+        # desenvolver lógica de extrato
+    elif opcao == 0:
+        print(f"Opção escolhida: {opcao}.")
+        break
+        # sair
+    else:
+        print("Opção inválida. Selecione novamente a opção desejada.")
+
