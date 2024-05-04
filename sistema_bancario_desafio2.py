f1523f9c1e1acfd84192a1a2d93263ad475421c7
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-05-04 20:14:04 -0300

    função criar conta

--- sistema_bancario_desafio2.py
+++ sistema_bancario_desafio2.py
@@ -79,12 +79,21 @@ def criar_usuario(usuarios):
 
     print("=== Usuário criado com sucesso! ===")
 
+
 def filtrar_usuario(cpf, usuarios):
     usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
     return usuarios_filtrados[0] if usuarios_filtrados else None
 
 
-# def criar_conta(agencia, numero_conta, usuarios):
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+    
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
 
 
 # def listar_contas(contas):
@@ -92,14 +101,15 @@ def filtrar_usuario(cpf, usuarios):
 
 def main():
     LIMITE_SAQUES = 3
-    
+    AGENCIA = 1
+
     saldo = 0
     extrato = ""
     valor_maximo_saque = 500
     numero_saques = 0
     usuarios = []
     contas = []
-    
+
     while True:
         opcao = int(menu())
 
@@ -127,6 +137,13 @@ def main():
 
         elif opcao == 4:
          criar_usuario(usuarios)
+
+        elif opcao == 5:
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
 
         elif opcao == 0:
             print(f"Opção escolhida: {opcao} - Sair.")
