9bebbf207f637d650d3ef8971ece21b377d22ebd
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-05-21 00:08:53 -0300

    classe Conta Corrente

--- sistema_bancario_desafio3_aula.py
+++ sistema_bancario_desafio3_aula.py
@@ -24,11 +24,11 @@ class PessoaFisica(Cliente):
 
 class Conta:
     def __init__(self, numero, cliente):
-        self.saldo = 0
-        self.numero = numero
-        self.agencia = "0001"
-        self.cliente = cliente
-        self.historico = Historico()
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
 
     @classmethod
     def nova_conta(cls, cliente, numero):
@@ -83,7 +83,36 @@ class Conta:
 
 
 class ContaCorrente(Conta):
-    pass
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self.limite = limite
+        self.limite_saques = limite_saques
+
+    def sacar(self, valor):
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes ir transacao["tipo"] == Saque.__name__]
+        )
+    
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saques >= self.limite_saques
+
+        if excedeu_limite:
+            print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+
+        elif excedeu_saques:
+            print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+
+        else:
+            return super().sacar(valor)
+        
+        return False
+    
+    def __str__(self):
+        return f"""\
+            Agência:\t{self.agencia}
+            C/c:\t\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
 
 
 class Historico:
