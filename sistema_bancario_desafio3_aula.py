7825a3821baab7522dc41f24b6d6e9318f676587
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-05-20 23:41:06 -0300

    classe Cliente

--- sistema_bancario_desafio3_aula.py
+++ sistema_bancario_desafio3_aula.py
@@ -6,7 +6,15 @@ class CLiente:
 
 
 class PessoaFisica(Cliente):
-    pass
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        pass
+
+    def adicionar_conta(self, conta):
+        pass
 
 class Conta:
     pass
