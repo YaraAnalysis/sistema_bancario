bb5f49fd549fcc72d0c6ba98c4af4095a2d7c9e9
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-05-20 23:39:18 -0300

    Create sistema_bancario_desafio3_aula.py

---
+++ sistema_bancario_desafio3_aula.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime
+
+class CLiente:
+    pass
+
+
+class PessoaFisica(Cliente):
+    pass
+
+class Conta:
+    pass
+
+
+class ContaCorrente(Conta):
+    pass
+
+
+class Historico:
+    pass
+
+
+class Transacao(ABC):
+    pass
+
+
+class Saque(Transacao):
+    pass
+
+
+class Deposito(Transacao):
+    pass
