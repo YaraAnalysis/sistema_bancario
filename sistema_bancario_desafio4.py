eb50f355fefc9381040381471d5273b60843f6ff
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2024-06-18 23:55:08 -0300

    Estabelece um limite de 10 transações diárias
    Estabelece um limite de 10 transações diárias para cada conta e exibe uma mensagem.

--- sistema_bancario_desafio4.py
+++ sistema_bancario_desafio4.py
@@ -18,8 +18,13 @@ class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
         self.contas = []
+        self.indice_conta = 0
 
     def realizar_transacao(self, conta, transacao):
+        if len(conta.historico.transacoes_do_dia()) >= 2:
+            print("\n@@@ Você escedeu o número de transações permitidas para hoje! @@@")
+            return
+        
         transacao.registrar(conta)
 
     def adicionar_conta(self, conta):
@@ -140,7 +145,7 @@ class Historico:
             {
                 "tipo": transacao.__class__.__name__,
                 "valor": transacao.valor,
-                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                "data": datetime.now(timezone.utc).strftime("%d-%m-%Y %H:%M:%S"),
             }
         )
 
